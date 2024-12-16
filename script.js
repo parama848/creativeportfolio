2df370acdd7e30503d185020616efb64e7103d1a
parama848 <parama8148497159@gmail.com>
2024-12-16 14:34:38 +0530

    feat : added all sources

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+var typed = new Typed ('.element', {
+    strings: ['Learning Frontend  ', 'Learning Backend ... '],
+    typeSpeed: 120,
+    backSpeed:20,
+    bakDelay:1600,
+    loop:true
+  });
+*/
+var typed = new Typed ('#name', {
+    strings: ['Hello!','I am','Paranthaman Renugopal'],
+    typeSpeed: 120,
+    backSpeed:20,
+    bakDelay:1600,
+    loop:false
+  });
